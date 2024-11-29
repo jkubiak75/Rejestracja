3e06daaf4d3b4cee848237841972318bc06101fb
9578802u <jkubiak31414@gmail.com>
2024-11-29 08:51:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/rejestracja/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.rejestracja;
+
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText email;
+    private EditText haslo;
+    private EditText haslo2;
+    private Button zatwierdz;
+    private TextView tekst;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        email = findViewById(R.id.email);
+        haslo = findViewById(R.id.haslo);
+        haslo2 = findViewById(R.id.haslo2);
+        zatwierdz = findViewById(R.id.zatwierdz);
+        tekst = findViewById(R.id.tekst);
+
+        haslo.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        haslo2.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+            zatwierdz.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String emaill = email.getText().toString().trim();
+                    String hasloo = haslo.getText().toString().trim();
+                    String powtorzHasloo = haslo2.getText().toString().trim();
+
+                    if (!emaill.contains("@"))
+                    {tekst.setText("Nieprawidłowy adres e-mail");
+                    }
+                    else if (!hasloo.equals(powtorzHasloo))
+                    {tekst.setText("Hasła się różnią");
+                    }
+                    else
+                    {tekst.setText("Witaj " + emaill);}
+                }
+            });
+        }
+    }
